78dee23e67f77d3477a3603a595fce0c01349267
Miguel O <miguelortiz15r@gmail.com>
2026-03-05 10:41:07 -0600

    123

---
+++ RecetArreWeb/Handlers/AuthorizationMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using RecetArreWeb.Services;
+using System.Net.Http.Headers;
+
+namespace RecetArreWeb.Handlers
+{
+    //DelegatingHandler: clase base que permite interceptar las solicitudes HTTP salientes y
+    //modificar el mensaje antes de que se envíe al servidor.
+    public class AuthorizationMessageHandler : DelegatingHandler
+    {
+        private readonly ITokenService tokenService;
+        public AuthorizationMessageHandler(ITokenService tokenService)
+        {
+            this.tokenService = tokenService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await tokenService.ObtenerToken();
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+
+    }
+}
--- RecetArreWeb/Program.cs
+++ RecetArreWeb/Program.cs
@@ -1,11 +1,39 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RecetArreWeb;
+using RecetArreWeb.Handlers;
+using RecetArreWeb.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Configurar HttpClient con handler de Autorizacion
+//TODO Descomentar
+builder.Services.AddScoped<AuthorizationMessageHandler>();
+
+builder.Services.AddScoped<HttpClient>(sp =>
+{
+    var handler = sp.GetRequiredService<AuthorizationMessageHandler>();
+    handler.InnerHandler = new HttpClientHandler();
+
+    return new HttpClient(handler)
+    {
+        BaseAddress = new Uri("https://localhost:7019/")
+    };
+});
+
+//registrar los servicios
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+// TODO: Todos los demás servicios ejemplo ICategoriaService, IRecetaService, etc
+
+//Configurar autenticación
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
 
 await builder.Build().RunAsync();
